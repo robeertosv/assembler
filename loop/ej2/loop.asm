88c5546e7357c36b413a5b05f00caed71c9af75c
Rober <121724164+robeertosv@users.noreply.github.com>
2024-02-18 13:59:00 +0100

    Add comments

--- loop/ej2/loop.asm
+++ loop/ej2/loop.asm
@@ -1,9 +1,11 @@
+# El programa pide un string, si es "S" sale de programa, si es otra cosa, lo convierte a minúsculas
+
 .data
 msg: .ascii "Introduce una frase: "
 len = .-msg
-buffer: .space 21
-frase: .space 20
-CL: .ascii "\n"
+buffer: .space 21 #Se almacena lo que llegue por teclado (Incluye el \n final)
+frase: .space 20 #Se almacena la frase a convertir
+CL: .ascii "\n" #Para hacer el salto de linea al final del todo (opcional)
 lcl = .-CL
 
 .text
@@ -11,52 +13,58 @@ lcl = .-CL
 .globl _start
 
 _start:
-
+#Muestra el mensaje "Introduce una frase: "
 movl $4, %eax
 movl $1, %ebx
 movl $msg, %ecx
 movl $len, %edx
 int $0x80
 
+#Lee lo que se ha tecleado
 movl $3, %eax
 movl $0, %ebx
 movl $buffer, %ecx
 movl $21, %edx
 int $0x80
 
-dec %eax
-movl %eax, %ecx
-movl %eax, %ebp
+#La función read() guarda en eax la longitud de la entrada
+dec %eax #Se resta 1 a esa longitud para quitar el \n
+movl %eax, %ecx # Necesario para usar loop
+movl %eax, %ebp # Como ecx va a cambiar y luego hará falta otro bucle, guardar eax en ebp
 movl $0, %esi
 jmp sanitize
 
+#Copiar el buffer en frase (ahora no incluye el \n)
 sanitize:
 movb buffer(%esi), %al
 movb %al, frase(%esi)
 inc %esi
 loop sanitize
 
+# Si la frase es la "S" salir, si no, pasar a minuscula
 cmpl $'S', frase
 je exit
 jne noEsLaS
 
 noEsLaS:
-movl %ebp, %ecx
+movl %ebp, %ecx #Set ecx a la longitud de la entrada anterior
 movl $0, %esi
 jmp transform
 transform:
 movb frase(%esi), %al
-orb $0x20, %al
+orb $0x20, %al # Máscara para pasar las letras a minúscula
 movb %al, frase(%esi)
 inc %esi
 loop transform
 
+#Mostrar la frase transformada
 movl $4, %eax
 movl $1, %ebx
 movl $frase, %ecx
 movl %ebp, %edx
 int $0x80
 
+#Hacer un salto de linea (opcional)
 movl $4, %eax
 movl $1, %ebx
 movl $CL, %ecx
