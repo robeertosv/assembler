749bfc1f2ba8c30a2fc5a3ecf47afd2a931f76cc
Rober <121724164+robeertosv@users.noreply.github.com>
2024-02-22 13:20:20 +0100

    Update ej.asm

--- fichero/ej.asm
+++ fichero/ej.asm
@@ -17,83 +17,83 @@
 
 _start:
 
-movl $4, %eax
-movl $1, %ebx
-movl $reqName, %ecx
-movl $lenReqName, %edx
-int $0x80
+    movl $4, %eax
+    movl $1, %ebx
+    movl $reqName, %ecx
+    movl $lenReqName, %edx
+    int $0x80
 
-movl $3, %eax
-movl $0, %ebx
-movl $name, %ecx
-movl $10, %edx
-int $0x80
+    movl $3, %eax
+    movl $0, %ebx
+    movl $name, %ecx
+    movl $10, %edx
+    int $0x80
 
-dec %eax
-movl %eax, %esi
+    dec %eax
+    movl %eax, %esi
+    
+    movl $4, %eax
+    movl $1, %ebx
+    movl $reqAge, %ecx
+    movl $lenReqAge, %edx
+    int $0x80
 
-movl $4, %eax
-movl $1, %ebx
-movl $reqAge, %ecx
-movl $lenReqAge, %edx
-int $0x80
+    movl $3, %eax
+    movl $0, %ebx
+    movl $age, %ecx
+    movl $10, %edx
+    int $0x80
+    
+    movl %eax, %edi
+    
+    movl $5, %eax
+    movl $dir, %ebx
+    movl $(01|02000|0100), %ecx
+    movl $0666, %edx
+    int $0x80
+    
+    movl %eax, %ebp
+    
+    movl $4, %eax
+    movl %ebp, %ebx
+    movl $name, %ecx
+    movl %esi, %edx
+    int $0x80
+    
+    movl $4, %eax
+    movl %ebp, %ebx
+    movl $TAB, %ecx
+    movl $1, %edx
+    int $0x80
+    
+    movl $4, %eax
+    movl %ebp, %ebx
+    movl $age, %ecx
+    movl %edi, %edx
+    int $0x80
+    
+    movl $6, %eax
+    movl %ebp, %ebx
+    int $0x80
+    
+    movl $4, %eax
+    movl $1, %ebx
+    movl $repMsg, %ecx
+    movl $lenRepMsg, %edx
+    int $0x80
+    
+    movl $3, %eax
+    movl $0, %ebx
+    movl $res, %ecx
+    movl $2, %edx
+    int $0x80
 
-movl $3, %eax
-movl $0, %ebx
-movl $age, %ecx
-movl $10, %edx
-int $0x80
-
-movl %eax, %edi
-
-movl $5, %eax
-movl $dir, %ebx
-movl $(01|02000|0100), %ecx
-movl $0666, %edx
-int $0x80
-
-movl %eax, %ebp
-
-movl $4, %eax
-movl %ebp, %ebx
-movl $name, %ecx
-movl %esi, %edx
-int $0x80
-
-movl $4, %eax
-movl %ebp, %ebx
-movl $TAB, %ecx
-movl $1, %edx
-int $0x80
-
-movl $4, %eax
-movl %ebp, %ebx
-movl $age, %ecx
-movl %edi, %edx
-int $0x80
-
-movl $6, %eax
-movl %ebp, %ebx
-int $0x80
-
-movl $4, %eax
-movl $1, %ebx
-movl $repMsg, %ecx
-movl $lenRepMsg, %edx
-int $0x80
-
-movl $3, %eax
-movl $0, %ebx
-movl $res, %ecx
-movl $2, %edx
-int $0x80
-
-mov $0, %esi
-cmpb $'S', res(%esi)
-je end
-jne _start
+    mov $0, %esi
+    cmpb $'S', res(%esi)
+    je end
+    jne _start
 
 end:
-movl $1, %eax
-movl $0, %ebx
-int $0x80
+    movl $1, %eax
+    movl $0, %ebx
+    int $0x80
